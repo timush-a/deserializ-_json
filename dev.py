a2e605d1bc6e45bace3e69d3de990bf966738004
Timush Aleksander <50115510+timush-a@users.noreply.github.com>
2020-03-02 19:53:55 +0200

    Update dev.py

--- dev.py
+++ dev.py
@@ -1,1 +1,12 @@
+import json, urllib.request
+users = urllib.request.urlopen("https://json.medrating.org/users").read()
+users_txt = open('users.txt', 'w')
+users_txt.write(str(json.loads(users)))
+
+
+todos = urllib.request.urlopen("https://json.medrating.org/todos").read()
+todos_txt = open('todos.txt', 'w')
+todos_txt.write(str(json.loads(todos)))
+
+
 
