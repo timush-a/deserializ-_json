6a124153fc085d359738c8b257f15574042e4b11
Timush Aleksander <50115510+timush-a@users.noreply.github.com>
2020-03-03 18:42:13 +0200

    Update dev.py
    1. Choose requests instead urllib.request
    2. Saving the response to a variable, not a file
    3. Create directory 'tasks'

--- dev.py
+++ dev.py
@@ -1,12 +1,45 @@
-import json, urllib.request
-users = urllib.request.urlopen("https://json.medrating.org/users").read()
-users_txt = open('users.txt', 'w')
-users_txt.write(str(json.loads(users)))
+import json
+import requests
+from datetime import datetime
+from os import getcwd, mkdir, path, chdir
 
 
-todos = urllib.request.urlopen("https://json.medrating.org/todos").read()
-todos_txt = open('todos.txt', 'w')
-todos_txt.write(str(json.loads(todos)))
+directory_name = "tasks"
+parent_dir = getcwd()
+path = path.join(parent_dir, directory_name)
+
+
+try:
+    mkdir(path)
+except OSError:
+    print('Folder already exists in ', parent_dir)
+
+chdir(path)
+now = datetime.now()
+date_time = now.strftime("%d.%m.%Y %H:%M")
+
+
+users_data_response = requests.get("https://json.medrating.org/users")
+tasks_data_responce = requests.get("https://json.medrating.org/todos")
+
+
+users = json.loads(users_data_response.text)
+tasks = json.loads(tasks_data_responce.text)
+
+
+# add user name, email address, report creation time, company adress
+
+user_info = ""
+for i in range(len(users)):
+    user_info += "{0}{1}{2}{3}".format(
+        users[i]['name'],                                   # user name
+        '{0}{1}{2}'.format('<', users[i]['email'], '>'),    # user email
+        date_time + '\n\n',                                 # creation time
+        'Competed tasks\n'                                  # company adress
+                                       )
+    if users[i]['id'] == tasks[i]['userId']:
+        for a in range(tasks[i]['id']):
+            print(a)
 
 
 
